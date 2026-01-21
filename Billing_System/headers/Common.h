f923195713f3be873f9dec37274f888ad8df4b04
satyendra <ysatyendra1409@gmail.com>
2026-01-21 20:14:18 +0530

    feat: add project structure and common header file

---
+++ Billing_System/headers/Common.h
@@ -0,0 +1,16 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+// Must include Windows before any C++ standard headers
+// to avoid std::byte conflict
+#include <windows.h>
+
+// Now include C++ standard library headers
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+#endif
